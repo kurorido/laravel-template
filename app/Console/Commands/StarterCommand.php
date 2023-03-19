854e706cb4d40736cc45a6396140dad76a37f089
kuro roli <kuroridoplayer@gmail.com>
2023-03-19 14:39:30 +0800

    fix

--- app/Console/Commands/StarterCommand.php
+++ app/Console/Commands/StarterCommand.php
@@ -32,7 +32,7 @@ class StarterCommand extends Command
         $PluralName = Str::plural($SnakeCase);
 
         // Create Migration & Model
-        Artisan::command("make:model -m $CamelCase");
+        Artisan::call("make:model -m $CamelCase");
 
         // Create CRUD Controller
         $string = file_get_contents(resource_path('stubs/StubController.php.stub'));
@@ -59,11 +59,12 @@ class StarterCommand extends Command
 
         $routes = file_get_contents($routesFile);
 
-        if (preg_match('/^use [A-Za-z\\\\]+;$/', $routes, $matches, PREG_OFFSET_CAPTURE)) {
+        if (preg_match('/^use [A-Za-z\\\\]+;/', $routes, $matches, PREG_OFFSET_CAPTURE)) {
             $pos = $matches[0][1];
 
             // Prepend a line before the line
-            $newRoutes = substr_replace($routes, "use App\Http\Controllers\{$CamelCase}Controller;\n", $pos, 0);
+            $import = implode("\\", ['App', 'Http', 'Controllers', "{$CamelCase}Controller;\n"]);
+            $newRoutes = substr_replace($routes, "use $import", $pos, 0);
 
             // Write the updated content back to the file
             file_put_contents($routesFile, $newRoutes);
@@ -77,6 +78,10 @@ class StarterCommand extends Command
         $string = str_replace("{SnakeCase}", $SnakeCase, $string);
         // Replace {PluralName}
         $string = str_replace("{PluralName}", $PluralName, $string);
+
+        if (!file_exists(app_path("Polices"))) {
+            mkdir(app_path("Polices"));
+        }
 
         file_put_contents(app_path("Polices/{$CamelCase}Policy.php"), $string);
     }
